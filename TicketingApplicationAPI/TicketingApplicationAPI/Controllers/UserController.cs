597ad2d5e417f5ff6c6368b33b9d20a704845015
SarahMartin1244 <c00257967@setu.ie>
2024-01-14 21:31:36 +0000

    Added a connection to the sql database in API

---
+++ TicketingApplicationAPI/TicketingApplicationAPI/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TicketingApplicationAPI.Context;
+
+namespace TicketingApplicationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+
+        public UserController(AppDbContext authContext)
+        {
+            _authContext = AppDbContext;
+        }
+    }
+}
